a04ede79247f16e5a1306a51afe463479055f2fd
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-29 23:27:55 -0400

    Complete project files.

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
 			}).then(response => response.json())
 			.then(response => {
 				if (response) {
-					fetch('https://cryptic-garden-13443.herokuapp.com:3000/image', {
+					fetch('https://cryptic-garden-13443.herokuapp.com/image', {
 						method: 'put',
 						headers: {'Content-Type': 'application/json'},
 						body: JSON.stringify({
